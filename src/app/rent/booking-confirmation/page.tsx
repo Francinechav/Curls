6d92b4b63e912fe349159be03765fc99a2c07641
Francinechav <fechavinda@gmail.com>
2025-12-03 10:23:25 +0200

    bb

--- src/app/rent/booking-confirmation/page.tsx
+++ src/app/rent/booking-confirmation/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 // src/app/rent/booking-confirmation/page.tsx
 import React from "react";
 import Image from "next/image";
