9e3a45586256d88f4294474a511a5028bdc48ffc
Francinechav <fechavinda@gmail.com>
2025-12-03 09:21:27 +0200

    try

--- src/app/payments/callback/page.tsx
+++ src/app/payments/callback/page.tsx
@@ -1,6 +1,6 @@
-"use client";
-
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const runtime = "edge";
 
 
 import { useEffect, useState } from "react";
