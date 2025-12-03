8bb01e68ca7522727ab0fd60bd33f8827757f010
Francinechav <fechavinda@gmail.com>
2025-12-03 23:21:55 +0200

    neww3

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-typescript :{
-         ignoreBuildErrors: true,
-        
-}
-
-
-
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["res.cloudinary.com"], // <--- add this
+  },
 };
 
 export default nextConfig;
